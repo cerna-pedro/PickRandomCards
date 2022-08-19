1756c080e0acacbc9541011ad1dc201a0f04ab51
Pedro Cerna <pedro.d.cerna@gmail.com>
2022-08-19 17:26:10 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿
+
+namespace PickRandomCards
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number of cards to pick: ");
+            string line = Console.ReadLine();
+            if (int.TryParse(line, out int numberOfCards))
+            {
+                string[] output = CardPicker.PickSomeCards(numberOfCards);
+                foreach (string card in output)
+                {
+                    Console.WriteLine(card);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Your input was wrong, get with the program :)");
+                
+            }
+        }
+    }
+}
